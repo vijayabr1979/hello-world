5b0c3491a136888fe4fe1ae4210bc8d60c20cb22
vijaya <vijayabr1979@gmail.com>
2023-12-06 19:21:48 +0530

    removed all lines except utf-8 line

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
-# This Python file uses the following encoding: utf-8
-print("feature-added")
-print("Hello, world")
-print("new change")
-print("Trying Jenkins integration")
-print("ngrok added for exposing localhost to the internet")
-print("still working on integration")
-print('added python3 in the jenkins-docker')
 print('utf-8 related converted CRLF to LF using notepad++')
