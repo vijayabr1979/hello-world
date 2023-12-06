9e13af5e637db271bef19ad42f0ec86c41e3956d
vijaya <vijayabr1979@gmail.com>
2023-12-06 18:15:16 +0530

    python3 added in jenkins-docker

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
 print("feature-added")
 print("Hello, world")
 print("new change")
 print("Trying Jenkins integration")
 print("ngrok added for exposing localhost to the internet")
 print("still working on integration")
+print('added python3 in the jenkins-docker')
